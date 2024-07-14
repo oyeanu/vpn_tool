2496ee4a7677c2495beab2614a2bb672c0e349cf
Ansar Mughal <skyline738@gmail.com>
2024-07-14 20:28:18 +0100

    Update NodeUnlockDetection.js

--- vpn_tool/Loon/Scripts/NodeUnlockDetection.js
+++ vpn_tool/Loon/Scripts/NodeUnlockDetection.js
@@ -38,7 +38,7 @@ let arrow = " ➟ "
 
 Promise.all([ytbTest(),disneyLocation(),nfTest(),daznTest(),parmTest(),discoveryTest(),gptTest()]).then(value => {
     let content = "------------------------------------</br>"+([result["Dazn"],result["Discovery"],result["Paramount"],result["Disney"],result["Netflix"],result["ChatGPT"],result["YouTube"]]).join("</br></br>")
-    content = content + "</br>------------------------------------</br>"+"<font color=#CD5C5C>"+"<b>节点</b> ➟ " + nodeName+ "</font>"
+    content = content + "</br>------------------------------------</br>"+"<font color=#CD5C5C>"+"<b>Node</b> ➟ " + nodeName+ "</font>"
     content =`<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + content + `</p>`
     console.log(content);
     $done({"title":result["title"],"htmlMessage":content})
